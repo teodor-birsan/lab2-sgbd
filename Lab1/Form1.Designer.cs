eac8c65d99c73f22f13978a26c03e7b5aefb8a1e
Teodor Birsan <teodorbirsan15@gmail.com>
2024-04-15 14:26:32 +0300

    Add project files.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.producatorGridView = new System.Windows.Forms.DataGridView();
+            this.instrumentGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.producatorGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instrumentGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // producatorGridView
+            // 
+            this.producatorGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.producatorGridView.Location = new System.Drawing.Point(32, 36);
+            this.producatorGridView.Name = "producatorGridView";
+            this.producatorGridView.RowHeadersWidth = 51;
+            this.producatorGridView.RowTemplate.Height = 24;
+            this.producatorGridView.Size = new System.Drawing.Size(338, 150);
+            this.producatorGridView.TabIndex = 0;
+            // 
+            // instrumentGridView
+            // 
+            this.instrumentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.instrumentGridView.Location = new System.Drawing.Point(32, 269);
+            this.instrumentGridView.Name = "instrumentGridView";
+            this.instrumentGridView.RowHeadersWidth = 51;
+            this.instrumentGridView.RowTemplate.Height = 24;
+            this.instrumentGridView.Size = new System.Drawing.Size(973, 234);
+            this.instrumentGridView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tabela Producatorilor";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tabela Instrumentelor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(795, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(106, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(410, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(106, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(416, 164);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(795, 192);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 8;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(441, 192);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 9;
+            this.buttonAdd.Text = "Adauga";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(557, 192);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "Sterge";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(656, 192);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(107, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Actualizeaza";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(546, 164);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(663, 164);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(663, 120);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(663, 76);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(663, 32);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Id Producator";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(410, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Model Instrument";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 16);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Pret";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(554, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 16);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Id Instrument";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(670, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 16);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Id Instrument";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(670, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 16);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Id Producator";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(670, 101);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 16);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Model";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(670, 145);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 16);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Pret";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(795, 144);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(164, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Nume Producator Selectat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 528);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.instrumentGridView);
+            this.Controls.Add(this.producatorGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.producatorGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instrumentGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView producatorGridView;
+        private System.Windows.Forms.DataGridView instrumentGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=DESKTOP-82UKUNB\SQLEXPRESS;Database=Magazin_Instrumente;
+        Integrated Security=true;TrustServerCertificate=true;";
+        DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter;
+        SqlDataAdapter childAdapter;
+        BindingSource bsParent;
+        BindingSource bsChild;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    //MessageBox.Show("Starea Conexiunii: " + conn.State.ToString());
+                    parentAdapter = new SqlDataAdapter("SELECT * FROM Producator;", conn);
+                    childAdapter = new SqlDataAdapter("SELECT * FROM Instrument;", conn);
+                    parentAdapter.Fill(ds, "Producator");
+                    childAdapter.Fill(ds, "Instrument");
+                    DataColumn pkColumn = ds.Tables["Producator"].Columns["id_prod"];
+                    DataColumn fkColumn = ds.Tables["Instrument"].Columns["id_prod"];
+                    DataRelation relation = new DataRelation("FK_Producator_Instrument", pkColumn,
+                    fkColumn);
+                    ds.Relations.Add(relation);
+                    bsParent = new BindingSource();
+                    bsChild = new BindingSource();
+                    bsParent.DataSource = ds.Tables["Producator"];
+                    producatorGridView.DataSource = bsParent;
+                    bsChild.DataSource = bsParent;
+                    bsChild.DataMember = "FK_Producator_Instrument";
+                    instrumentGridView.DataSource = bsChild;
+                    textBox1.DataBindings.Add("Text", bsParent, "nume_prod", true);
+                    textBox2.DataBindings.Add("Text", bsParent, "id_prod", true);
+                    textBox5.DataBindings.Add("Text", bsChild, "id_inst", true);
+                    textBox9.DataBindings.Add("Text", bsChild, "id_inst", true);
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    parentAdapter.SelectCommand.Connection = connection;
+                    childAdapter.SelectCommand.Connection = connection;
+                    ds.Tables["Instrument"].Clear();
+                    ds.Tables["Producator"].Clear();
+                    parentAdapter.Fill(ds, "Producator");
+                    childAdapter.Fill(ds, "Instrument");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (producatorGridView.CurrentRow != null)
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string querry = "IF EXISTS(SELECT * FROM Producator WHERE id_prod = @cod_prod)" +
+                            "BEGIN " +
+                            "IF @pret > 0 " +
+                            "BEGIN " +
+                            "INSERT INTO Instrument (id_prod, model_instrument, pret_instrument) VALUES (@cod_prod, @model, @pret) " +
+                            "END " +
+                            "END ";
+                        using (SqlCommand command = new SqlCommand(querry, connection))
+                        {
+                            command.Parameters.AddWithValue("@cod_prod", textBox2.Text);
+                            command.Parameters.AddWithValue("@model", textBox3.Text);
+                            command.Parameters.AddWithValue("@pret", textBox4.Text);
+
+                            int result = command.ExecuteNonQuery();
+                            if(result > 0) {
+                                MessageBox.Show("Instrumentul a fost adaugat cu succes.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Instrumentul nu a fost adaugat.");
+                            }
+                        }
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                buttonRefresh_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Selectați un producator pentru a adăuga un instrument.");
+            }
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (instrumentGridView.CurrentRow != null)
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string querry = "IF EXISTS(SELECT * FROM Instrument WHERE id_inst = @cod_inst) " +
+                            "BEGIN " +
+                            "DELETE FROM Instrument WHERE id_inst = @cod_inst " +
+                            "END";
+                        using (SqlCommand command = new SqlCommand(querry, connection))
+                        {
+                            command.Parameters.AddWithValue("@cod_inst", textBox5.Text);
+                            int res = command.ExecuteNonQuery();
+                            if(res > 0)
+                            {
+                                MessageBox.Show("Instrumentul a fost sters cu succes.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Instrumentul cu id-ul dat nu exista.");
+                            }
+                        }
+                        connection.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                buttonRefresh_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Nu exista instrumente.");
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if(instrumentGridView.CurrentRow != null && producatorGridView.CurrentRow != null)
+            {
+                try
+                {
+                    using(SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string querry = "IF EXISTS(SELECT * FROM Instrument WHERE id_inst = @cod_inst) " +
+                            "BEGIN " +
+                            "IF EXISTS(SELECT * FROM Producator WHERE id_prod = @cod_prod) " +
+                            "BEGIN " +
+                            "IF @pret > 0 " +
+                            "BEGIN " +
+                            "UPDATE Instrument SET id_prod = @cod_prod, model_instrument = @model, pret_instrument = @pret WHERE id_inst = @cod_inst " +
+                            "END " +
+                            "END " +
+                            "END";
+                        using (SqlCommand command = new SqlCommand(querry,connection))
+                        {
+                            command.Parameters.AddWithValue("@cod_inst", textBox9.Text);
+                            command.Parameters.AddWithValue("@cod_prod", textBox8.Text);
+                            command.Parameters.AddWithValue("@model", textBox7.Text);
+                            command.Parameters.AddWithValue("@pret", textBox6.Text);
+                            int res = command.ExecuteNonQuery();
+                            if (res > 0)
+                            {
+                                MessageBox.Show("Instrumentul a fost actualizat cu succes.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Actualizarea instumentului a esuat.");
+                            }
+                        }
+                        connection.Close();
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                buttonRefresh_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Nu exista instrumente sau producatori.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
